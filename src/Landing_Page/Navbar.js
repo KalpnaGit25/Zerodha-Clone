43327e9e5de78c6a34a2a3bc9b19301725c64b42
Kalpna <kalpnak030@gmail.com>
2026-07-22 13:05:15 +0530

    Update Zerodha Clone

--- src/Landing_Page/Navbar.js
+++ src/Landing_Page/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     
       <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom" style={{ backgroundColor:"#FFF"}}>
         <div class="container p-2">
-          <a class="navbar-brand" href="#">
+          <Link class="navbar-brand" to="/">
             <img src="media/images/logo.svg "  style={{width:"25%"}}  alt="Logo"/>
-          </a>
+          </Link>
           <button
             class="navbar-toggler"
             type="button"
@@ -24,33 +25,33 @@ function Navbar() {
         <form class="d-flex" role="search">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+                <Link class="nav-link active" aria-current="page" to="/signup">
+                  SignUp
+                </Link>
 
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#">
-                    About
-                </a>
+                <Link class="nav-link active" aria-current="page" to="/about">
+                  About
+                </Link>
                 </li>
 
                  <li class="nav-item">
-                <a class="nav-link active " href="#">
-                    Product
-                </a>
+              <Link class="nav-link active" aria-current="page" to="/products">
+                 Product
+                </Link>
                 </li>
 
                  <li class="nav-item">
-                <a class="nav-link active" href="#">
-                    Pricing
-                </a>
+               <Link class="nav-link active" aria-current="page" to="/pricing">
+                  Pricing
+                </Link>
                 </li>
 
                  <li class="nav-item">
-                <a class="nav-link active" href="#">
-                    Support
-                </a>
+                <Link class="nav-link active" aria-current="page" to="/support">
+                 Support
+                </Link>
                 </li>
                  
                
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import HomePage from './Landing_Page/home/HomePage';
+import SignUp  from './Landing_Page/signup/SignUp';
+import AboutPage  from './Landing_Page/about/AboutPage';
+import ProductPage  from './Landing_Page/products/ProductPage';
+import PricingPage  from './Landing_Page/pricing/PricingPage';
+import SupportPage  from './Landing_Page/support/SupportPage';
+
+import Navbar  from './Landing_Page/Navbar';
+import Footer  from './Landing_Page/Footer';
+import NotFound from './Landing_Page/NotFound';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <Navbar />   
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/signup" element={<SignUp/>} />
+    <Route path="/about" element={<AboutPage />} />
+    <Route path="/products" element={<ProductPage />} />
+    <Route path="/pricing" element={<PricingPage />} />
+     <Route path="/support" element={<SupportPage />} />
+     <Route path="*" element={<NotFound />} />
 
-      < HomePage />
-   
-  </React.StrictMode>
+  </Routes>
+  <Footer />
+    
+  </BrowserRouter>
 );
