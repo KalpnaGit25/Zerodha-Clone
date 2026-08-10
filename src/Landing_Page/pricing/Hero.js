fdb3a55cba0dbb42e45da5460ebc1270512c9500
Kalpna <kalpnak030@gmail.com>
2026-08-10 23:51:27 +0530

    Updating Project

--- src/Landing_Page/pricing/Hero.js
+++ src/Landing_Page/pricing/Hero.js
@@ -4,7 +4,38 @@ import React from 'react'
 function Hero() {
     return (
      
-     <h1>Hero</h1>
+          <div className="container ">
+       
+
+         <div className="row p-5 mt-5 border-bottom text-center ">
+            <h1>Pricing</h1>
+            <h3 className="text-muted mt-3 fs-5">Free equity investments and flat ₹20 traday and F&O traders</h3>  
+            </div>
+
+            <div className="row p-5 mt-5">
+                <div className="col-4 p-5">
+                   <img src="media/images/pricingEquity.svg" />
+                   <h1>Free equity delivery  </h1>
+                   <p className="text-muted">
+                    All equity delivery investments (NSE, BSE), ace absolutely free - ₹ 0 brokerage.
+                   </p>
+                </div>
+                   
+                <div className="col-4 p-5">
+                     <img src="" />
+                   <h1></h1>
+                   <p></p>
+                </div>
+                <div className="col-4 p-5">
+                     <img src="" />
+                   <h1></h1>
+                   <p></p>
+                </div>
+                
+            
+            </div>
+
+        </div>
     )
 }
 
