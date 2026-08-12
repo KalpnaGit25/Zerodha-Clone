5a24ad27d491625234a262a025e1e74f8c5c3117
Kalpna <kalpnak030@gmail.com>
2026-08-12 23:53:44 +0530

    Updates in project

--- src/Landing_Page/support/CreateTicket.js
+++ src/Landing_Page/support/CreateTicket.js
@@ -4,7 +4,25 @@ import React from 'react'
 function CreateTicket() {
     return (
      
-     <h1>CreateTicket</h1>
+     <div className="container">
+        <div className="row p-5 mt-5 mb-5">
+            <h1 className="fs-2 text-center ">
+                To create a ticket, select a relevent topic
+            </h1>
+              <div className="container">
+               <div className="col-4 mt-5 mb-5">
+                  <h4 className=" ">
+                 <i class="fas fa-plus-circle"></i> Account Opening
+                  </h4>
+            </div>
+            <div className="col-4 mt-5 mb-5">
+                  <h4 className=" ">
+                    To create a ticket, select a relevent topic
+                  </h4>
+            </div>
+          </div>
+        </div>
+    </div>
     )
 }
 
