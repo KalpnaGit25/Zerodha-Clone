b5597a0a3de05ff4c60699fbfe970a51245cb283
Kalpna <kalpnak030@gmail.com>
2026-08-15 23:49:08 +0530

    Updates Project..

--- src/Landing_Page/support/Hero.js
+++ src/Landing_Page/support/Hero.js
@@ -4,7 +4,36 @@ import React from 'react'
 function Hero() {
     return (
      
-     <h1>Hero</h1>
+         <section className="container-fluid" id="supportHero">
+        <div className=" p-5 mt-5 mb-5" id="spportWrapper">
+            <h4>Support Portal</h4>
+            <a href="">Track Tickets</a>
+          </div>
+
+           <div className=" row  p-5 mt-5 mb-5" > 
+             <div className=" col-6 p-5 " >
+                <h1 className="fs-3">Search for an answer or browser help topics to create a ticket</h1>
+                <input placeholder="Eg. how do i activate F&O, why is my getting rejected."  />
+
+               <a href=""> Track account Opening</a>
+               <a href="">Track segment activation</a>  
+               <a href="">Intraday margins</a>
+               <a href="">Kite user manual</a> 
+
+             </div>
+             <div className="col-6 p-5" > 
+                <h1 className="fs-3">Featured</h1>
+                <ol>
+
+                
+
+
+                </ol>
+                <a href=""> Current Takeovers and Delisting - January 2024</a>
+               <a href="">Latest Intraday levengers manual</a> 
+             </div>
+          </div>
+        </section>
     )
 }
 
