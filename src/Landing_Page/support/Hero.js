b160ef50f68a61b76f190fd3412534000f7f0f3a
Kalpna <kalpnak030@gmail.com>
2026-08-16 13:30:26 +0530

    Frontend update

--- src/Landing_Page/support/Hero.js
+++ src/Landing_Page/support/Hero.js
@@ -5,15 +5,15 @@ function Hero() {
     return (
      
          <section className="container-fluid" id="supportHero">
-        <div className=" p-5 mt-5 mb-5" id="spportWrapper">
+        <div className=" p-5 " id="spportWrapper">
             <h4>Support Portal</h4>
             <a href="">Track Tickets</a>
           </div>
 
-           <div className=" row  p-5 mt-5 mb-5" > 
-             <div className=" col-6 p-5 " >
+           <div className=" row  p-5 m-3" > 
+             <div className=" col-6 p-3 " >
                 <h1 className="fs-3">Search for an answer or browser help topics to create a ticket</h1>
-                <input placeholder="Eg. how do i activate F&O, why is my getting rejected."  />
+                <input placeholder="Eg. how do i activate F&O, why is my getting rejected."  /><br/>
 
                <a href=""> Track account Opening</a>
                <a href="">Track segment activation</a>  
@@ -21,16 +21,13 @@ function Hero() {
                <a href="">Kite user manual</a> 
 
              </div>
-             <div className="col-6 p-5" > 
+             <div className="col-6 p-3" > 
                 <h1 className="fs-3">Featured</h1>
                 <ol>
-
-                
-
-
+                  <li><a href=""> Current Takeovers and Delisting - January 2024</a></li>
+                    <li><a href="">Latest Intraday levengers manual</a></li> 
                 </ol>
-                <a href=""> Current Takeovers and Delisting - January 2024</a>
-               <a href="">Latest Intraday levengers manual</a> 
+               
              </div>
           </div>
         </section>
