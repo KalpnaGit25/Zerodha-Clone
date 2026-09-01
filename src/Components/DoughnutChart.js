ff17756a51e82ec466f85fb5e8c8df65d5c65d9e
Kalpna <kalpnak030@gmail.com>
2026-09-01 23:49:11 +0530

    Create DashBoard react app

---
+++ src/Components/DoughnutChart.js
@@ -0,0 +1,5 @@
+import { Doughnut } from "react-chartjs-2";
+
+export const DoughnutChart = ({ data }) => {
+  return <Doughnut data={data} />;
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from "./Components/Home";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <BroserRouter>
+   <Routes>
+    <Route path="/" element={<Home />}    />
+   </Routes>
+   </BroserRouter>
   </React.StrictMode>
 );
 
